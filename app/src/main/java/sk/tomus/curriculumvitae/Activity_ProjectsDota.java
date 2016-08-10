76286d799c6f7f625cf7e632f806117a6ff42b52
Tomus <tomuso.o@gmail.com>
2016-08-10 10:58:07 +0200

    filling Interests

--- app/src/main/java/sk/tomus/curriculumvitae/Activity_ProjectsDota.java
+++ app/src/main/java/sk/tomus/curriculumvitae/Activity_ProjectsDota.java
@@ -23,7 +23,7 @@ public class Activity_ProjectsDota extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_experience);
+        setContentView(R.layout.layout_dota);
         loadBackdrop();
         initToolbar();
         initNavigationDrawer();
